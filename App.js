63f939db9ed7aff7f962d6030676426af8d08bde
srdevaraj2006 <srlaladev@gmail.com>
2025-07-12 12:40:02 +0530

    making changes to learn the flexD

--- App.js
+++ App.js
@@ -3,62 +3,21 @@ import { Button, StyleSheet, Text, TextInput, View ,Pressable} from 'react-nativ
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.v1}>
-        <StatusBar style="auto" />
-      <TextInput
-        style = {styles.TextInput}
-        placeholder='Enter your name :'
-        placeholderTextColor={'grey'}
-        secureTextEntry
-        multiline
-        numberOfLines={2}
-        autoFocus
-      />
-      
-      <Button
-        title='Click here'
-        color={"red"}
-        onPress={()=>alert('Button pressed...')}
-      />
-      <Pressable
-        onPress={() => console.log('Pressed')}
-        style={({ pressed }) => [
-        {
-          backgroundColor: pressed ? 'pink' : 'red',
-          padding: 10,
-         
-          borderRadius: 20
-        },
-        ]}
-      >
-  <Text style={{ color: 'white', fontWeight: 'bold' }}>Click Me</Text>
-</Pressable>
+      <View style={styles.v2}>
+      <StatusBar style="auto"/>
+        <Text style={{color:'white'}}>Left</Text>
+        <Text style={{color:'white'}}>Right</Text>
       </View>
-      <View>
-        <Text>This text is in 2nd View...</Text>
-      </View>
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  TextInput:{
-    justifyContent:'center',
-    textAlign:'center',
-    fontSize:20,
-    color:'black',
-    padding:10,
-    backgroundColor:'white'
-  },
-  v1:{
+  v2:{
+    flexDirection:'row',
+    width:'100%',
+    padding:20,
     backgroundColor:'blue',
-    color:'white'
+    justifyContent:'center',
+    alignItems:'center',
   }
 });
