0c9d050e1fb1e908e46d39de5a70c84ec460a6f1
srdevaraj2006 <srlaladev@gmail.com>
2025-07-12 15:15:41 +0530

    uri image learning in RN

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View ,Pressable} from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View ,Pressable ,Image} from 'react-native';
 
 export default function App() {
   return (
-      <View style={styles.contianer}>
+    <View style={styles.contianer}>
       <StatusBar style="auto"/>
-        <View style={styles.v2}>
-          <Text style={{color:'white'}}>Left</Text>
-          <Text style={{color:'white'}}>Right</Text>
-        </View>
+      <Image
+        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+        style={{ width: 100, height: 100 }}
+      />
+      <View style={styles.v2}>
+        <Text style={{color:'white'}}>Left</Text>
+        <Text style={{color:'white'}}>Right</Text>
       </View>
+    </View>
   );
 }
 
