577246cbd8fc4a2aa069e170404f98a6749fb55d
srdevaraj2006 <srlaladev@gmail.com>
2025-07-12 11:40:49 +0530

    learining c2

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View ,Pressable} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.v1}>
+        <StatusBar style="auto" />
+      <TextInput
+        style = {styles.TextInput}
+        placeholder='Enter your name :'
+        placeholderTextColor={'grey'}
+        secureTextEntry
+        multiline
+        numberOfLines={2}
+        autoFocus
+      />
+      
+      <Button
+        title='Click here'
+        color={"red"}
+        onPress={()=>alert('Button pressed...')}
+      />
+      <Pressable
+        onPress={() => console.log('Pressed')}
+        style={({ pressed }) => [
+        {
+          backgroundColor: pressed ? 'pink' : 'red',
+          padding: 10,
+         
+          borderRadius: 20
+        },
+        ]}
+      >
+  <Text style={{ color: 'white', fontWeight: 'bold' }}>Click Me</Text>
+</Pressable>
+      </View>
+      <View>
+        <Text>This text is in 2nd View...</Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +49,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  TextInput:{
+    justifyContent:'center',
+    textAlign:'center',
+    fontSize:20,
+    color:'black',
+    padding:10,
+    backgroundColor:'white'
+  },
+  v1:{
+    backgroundColor:'blue',
+    color:'white'
+  }
 });
