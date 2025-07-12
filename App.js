28d1ba789d6baf88421832367eadadb2fba7bf59
srdevaraj2006 <srlaladev@gmail.com>
2025-07-12 13:02:12 +0530

    flex class learning in RN

--- App.js
+++ App.js
@@ -3,21 +3,30 @@ import { Button, StyleSheet, Text, TextInput, View ,Pressable} from 'react-nativ
 
 export default function App() {
   return (
-      <View style={styles.v2}>
+      <View style={styles.contianer}>
       <StatusBar style="auto"/>
-        <Text style={{color:'white'}}>Left</Text>
-        <Text style={{color:'white'}}>Right</Text>
+        <View style={styles.v2}>
+          <Text style={{color:'white'}}>Left</Text>
+          <Text style={{color:'white'}}>Right</Text>
+        </View>
       </View>
   );
 }
 
 const styles = StyleSheet.create({
+  contianer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   v2:{
     flexDirection:'row',
-    width:'100%',
-    padding:20,
     backgroundColor:'blue',
+    padding: 20,
+    width: '50%',
+    height: 200,
+    alignItems: 'center',
     justifyContent:'center',
-    alignItems:'center',
   }
 });
